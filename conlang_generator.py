4b9ec307f7e3cd81340efbd46437831f696a0129
Oleg <48214249+OlegKorn@users.noreply.github.com>
2019-03-08 00:45:11 +0300

    Update conlang_generator.py

--- conlang_generator.py
+++ conlang_generator.py
@@ -3,8 +3,10 @@ import random
 import string    
 
 
-VOWS = ['aeyuio']
-CONS = ['qzwsxdcrfvtgbhnjmklp']
+VOWS = ['a','e','y','u','i','o']
+CONS = ['q','z','w','s','x','d','c','r','f','v','t','g','b','h','n','j','m','k','l','p']
+
+
 class Morphem: 
     '''
     morphem has params:
@@ -17,29 +19,54 @@ class Morphem:
     '''
     def __init__(self, graphems_set, counter=True):
         self.graphems_set = graphems_set
+        self.graphems_list = list(self.graphems_set)
         self.counter = len(graphems_set)
   
 
     def show_info(self):
         print('graphems_set is {}\n'
           'len(graphems_set) is {}' 
-          .format(self.graphems_set, self.counter)            
+          .format(self.graphems_list, self.counter)            
         )        
   
+    
+    #generate the needed random letters in accordance with the passed in ones
+    def generate(self):
+        '''
+        generates morhpem after checking the type out
+        (appends grapheme of needed type to a list * indicator) times     
+        '''
+        
+        self.pre_output = ''
+        
+        #define the type of each graphem
+        while self.counter != 0:
 
-    '''def generate(self):
-        for i in graphems_set:
-'''
+            for i in self.graphems_list:
+
+                #if code == "vowel" 
+                if i == 'v':
+                    v = random.choice(VOWS)
+                    self.pre_output += v
+
+                #if code == "consonant" 
+                elif i == 'c':
+                    c = random.choice(CONS)
+                    self.pre_output += c
+                self.counter -= 1
+
+        self.output = list(self.pre_output) 
 
 
 
-m = Morphem('vvc')
-m1 = Morphem('vvcvc')
-m.show_info()
-m1.show_info()
+    def get_output(self):
+        print(self.output)
+        print(self.graphems_list)
 
 
-
+m = Morphem('vcvc')
+m.generate()
+m.get_output()
 
 
 

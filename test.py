3b657200f247026407786726a70d7a0b0008652b
Oleg <48214249+OlegKorn@users.noreply.github.com>
2019-03-10 12:16:24 +0300

    Update test.py
    for i in range(m_len):
            graphem_type = random.choice(TYPES)
            generated_type += graphem_type
    
        for i in generated_type:
            if i == 'v':
                v_choosen = random.choice(VOWS)
                generated_morph += v_choosen
            elif i == 'c':
                c_choosen = random.choice(CONS)
                generated_morph += c_choosen
    
        res.append(generated_morph)
        print(res)

--- test.py
+++ test.py
@@ -7,13 +7,14 @@ CONS = ['q','z','w','s','kh','d','c','r',
    'm','k','l','p','ʧ','θ','ð','z','ň' 
 ]
 
-#to increase the depth of choice there are more than 2 same entities here
+#to increase the "depth of choice" there are more than 2 same entities here
 TYPES = ['v','c','v','c','v','c','v','c','v','c','v','c','v','c']
 res = []
 
 #
 
 def set_random_len():
+    
     l_min = random.choice(range(2))
     l_max = random.choice(range(2,5))
 
@@ -22,11 +23,29 @@ def set_random_len():
 
 
 def generate_morph():
+    
+    generated_type = ''
+    generated_morph = '' 
+    
     m_len = set_random_len()
-    graphem_type = random.choice(TYPES)
-    print(graphem_type) 
+        
     for i in range(m_len):
-        print(f'i is {i}; m_len is {m_len}')
+        graphem_type = random.choice(TYPES)
+        generated_type += graphem_type
+
+    for i in generated_type:
+        if i == 'v':
+            v_choosen = random.choice(VOWS)
+            generated_morph += v_choosen
+        elif i == 'c':
+            c_choosen = random.choice(CONS)
+            generated_morph += c_choosen
+
+    res.append(generated_morph)
+    print(res)
+
+        
+
     
 
 

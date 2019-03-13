c64a4b68deeec7ec3c6d16badce6cef67bcaed69
O <48214249+OlegKorn@users.noreply.github.com>
2019-03-13 11:48:08 +0300

    Create conlang_data.py

---
+++ conlang_data.py
@@ -0,0 +1,27 @@
+#prepositions data
+PREPOS = [
+    'at', 'in', 'on', 'upon', 'of',
+    'with', 'against','among','by'
+]
+
+#preffixes data
+PVOWS = ['a','o']
+PCONS = ['r','v','rr','m','ð']
+PREF_TYPES = ['vc', 'cv']
+
+#roots data
+ROOT_TYPES = [
+  'cvvc', 'vcvvc', 'cv', 'cvv', 'vvcv', 
+  'cvcv', 'vcvc', 'ccv', 'vvc', 'vc'
+]
+RVOWS = ['a','e','y','u','i','o', 'æ', 'ā', 'ī']
+RCONS = [
+  's','k','d','rr','n', 
+  'g','t','f','rr','b','n',
+  'm','k','rr','l','p','ð' 
+]
+
+#suffixes data
+SUFF_TYPES = ['vc']
+SVOWS = ['a','æ','ā']
+SCONS = ['ss','p','ð']
